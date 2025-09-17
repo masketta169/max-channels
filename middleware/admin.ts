2194d9c54508fbaa29b71998a0a2c2f1bef85420
Андрей Беляев <andrejbelaev@MacBook-Air-Andrej.local>
2025-09-17 22:33:46 +0500

    2131

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,7 +1,9 @@
+// middleware/admin.global.ts
 import { jwtDecode } from "jwt-decode";
 import { getCookie } from '@/utils/cookies'
 
 export default defineNuxtRouteMiddleware((to) => {
+  const token2 = getCookie("accessToken");
 
   const event = useRequestEvent();
   const cookies = event?.req?.headers?.cookie || "";
@@ -12,6 +14,8 @@ export default defineNuxtRouteMiddleware((to) => {
     .find(c => c.startsWith("accessToken="))
     ?.split("=")[1];
 
+  console.log('token', token);
+  console.log('token2', token2);
 
   if (!token) {
     console.log("[ADMIN MIDDLEWARE] Нет токена, редирект на /");
@@ -20,9 +24,13 @@ export default defineNuxtRouteMiddleware((to) => {
 
   try {
     const decoded = jwtDecode<{ role?: string }>(token);
+    // console.log(decoded);
+    // console.log(decoded.role);
+    // console.log(token)
+    // console.log("[ADMIN MIDDLEWARE] Декодированный токен:", decoded);
 
     if (decoded.role !== "ADMIN") {
-      console.log("[ADMIN MIDDLEWARE] Роль не ADMIN, редирект на /");
+      // console.log("[ADMIN MIDDLEWARE] Роль не ADMIN, редирект на /");
       return navigateTo("/");
     }
 
