bb04bca074bddd8b231e279ed6adf7e07406c75c
Андрей Беляев <andrejbelaev@MacBook-Air-Andrej.local>
2025-09-17 22:31:02 +0500

    123123

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,9 +1,7 @@
-// middleware/admin.global.ts
 import { jwtDecode } from "jwt-decode";
 import { getCookie } from '@/utils/cookies'
 
 export default defineNuxtRouteMiddleware((to) => {
-  const token2 = getCookie("accessToken");
 
   const event = useRequestEvent();
   const cookies = event?.req?.headers?.cookie || "";
@@ -14,28 +12,22 @@ export default defineNuxtRouteMiddleware((to) => {
     .find(c => c.startsWith("accessToken="))
     ?.split("=")[1];
 
-  console.log('token', token);
-  console.log('token2', token2);
 
   if (!token) {
     console.log("[ADMIN MIDDLEWARE] Нет токена, редирект на /");
-    // return navigateTo("/");
+    return navigateTo("/");
   }
 
   try {
     const decoded = jwtDecode<{ role?: string }>(token);
-    console.log(decoded);
-    console.log(decoded.role);
-    console.log(token)
-    // console.log("[ADMIN MIDDLEWARE] Декодированный токен:", decoded);
 
     if (decoded.role !== "ADMIN") {
-      // console.log("[ADMIN MIDDLEWARE] Роль не ADMIN, редирект на /");
+      console.log("[ADMIN MIDDLEWARE] Роль не ADMIN, редирект на /");
       return navigateTo("/");
     }
 
   } catch (e) {
     console.error("[ADMIN MIDDLEWARE] Ошибка парсинга токена:", e);
-    // return navigateTo("/");
+    return navigateTo("/");
   }
 });
